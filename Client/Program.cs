e5a7659052a9b09da4f2b3d7bbb2b9d671f0d4c1
Optifun <frdima19@gmail.com>
2022-03-27 14:11:15 +0700

    Cleanup Client & add MudBlazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
 using online_shop.Client;
 using System;
 using System.Net.Http;
@@ -8,6 +10,7 @@ using System.Net.Http;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
