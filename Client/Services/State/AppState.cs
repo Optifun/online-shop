f0009c81d4dd80cf2ef0a8c5feb3c42fdf84362b
Optifun <frdima19@gmail.com>
2022-04-18 15:15:55 +0700

    Add UsersPanel

--- Client/Services/State/AppState.cs
+++ Client/Services/State/AppState.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OnlineShop.Core.DTO;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OnlineShop.Client.Services.State
 {
@@ -19,6 +22,17 @@ namespace OnlineShop.Client.Services.State
         {
             UserState = state;
             StateChanged?.Invoke();
+        }
+
+        public async Task<List<UserData>> FetchUsers()
+        {
+            return await Task.CompletedTask.ContinueWith((t) => new List<UserData>()
+            {
+                new UserData(0, "ASD", false, DateTime.Today),
+                new UserData(1, "ASDW", true, DateTime.Today),
+                new UserData(2, "ASDD", false, DateTime.Today),
+                new UserData(3, "ASDC", false, DateTime.Today),
+            });
         }
     }
 }
