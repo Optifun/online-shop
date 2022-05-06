69ec5546b90da11e77e6adbc74add37767337be1
Optifun <frdima19@gmail.com>
2022-05-06 23:05:04 +0700

    Fix ProductEditor: apply Vendor and Category changes

---
+++ Client/CollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineShop.Client
+{
+    public static class CollectionExtensions
+    {
+        public static IList<T> Replace<T>(this IList<T> collection, T old, T @new)
+        {
+            int id = collection.IndexOf(old);
+            if (id == -1)
+                return collection;
+
+            collection.RemoveAt(id);
+            collection.Insert(id, @new);
+            return collection;
+        }
+    }
+}
