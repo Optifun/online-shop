412105fd093570b8501353fec1a6895dbfb70b31
Optifun <frdima19@gmail.com>
2022-05-06 22:00:00 +0700

    Update Price: add mutable Price

--- Core/DTO/Price.cs
+++ Core/DTO/Price.cs
@@ -1,15 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace OnlineShop.Core.DTO
 {
-    public record Price(long Id, float Value, float Discount)
+    public interface IPrice
     {
-        public static Price Convert(PriceMutable mutable) =>
-            new Price(mutable.Id ?? -1, mutable.Value, mutable.Discount);
+        float Value { get; }
+        float Discount { get; }
+        public float ApplyDiscount();
     }
 
-    public class PriceMutable
+    public record Price(long Id, float Value, float Discount) : IPrice
     {
+        public static Price Convert(PriceMutable mutable) => 
+            new Price(mutable.Id ?? -1, mutable.Value, mutable.Discount);
+        
+        public float ApplyDiscount() => 
+            Value * (100 - Discount) / 100;
+    }
+
+    public class PriceMutable : IPrice
+    {
+        public long? Id { get; set; }
+        [Required, Range(0, 9_999_999.99)] public float Value { get; set; }
+
+        [Required, Range(0, 99.99)] public float Discount { get; set; }
+        
         public PriceMutable(long Id, float Value, float Discount)
         {
             this.Id = Id;
@@ -25,9 +41,7 @@ namespace OnlineShop.Core.DTO
         {
         }
 
-        public long? Id { get; set; }
-        [Required, Range(0, 9_999_999.99)] public float Value { get; set; }
-
-        [Required, Range(0, 99.99)] public float Discount { get; set; }
+        public float ApplyDiscount() => 
+            Value * (100 - Discount) / 100;
     }
 }
