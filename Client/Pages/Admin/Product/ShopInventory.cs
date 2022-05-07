9414c09695e92f3801fc181c4d44a88d2cfc5cbd
Optifun <frdima19@gmail.com>
2022-05-07 15:11:42 +0700

    Update ProductEditor: add Product inventory

---
+++ Client/Pages/Admin/Product/ShopInventory.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShop.Core.DTO;
+
+namespace OnlineShop.Client.Pages.Admin.Product
+{
+    public class ShopInventory
+    {
+        public Shop Shop;
+        public long Amount;
+
+        public void Deconstruct(out Shop shop, out long amount)
+        {
+            shop = Shop;
+            amount = Amount;
+        }
+    }
+}
